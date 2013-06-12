e0ee3af08fc75f561c7ee9320a98f29c165e8f5d
Joaquin <joaquin@chimeces.com>
2013-06-12 20:23:32 +0300

    Update versions of clojure and cljsbuild
    I've updated the versions to the newer ones right now. I've tested it locally and it works without problems.

--- src/leiningen/new/cljs_kickoff/project.clj
+++ src/leiningen/new/cljs_kickoff/project.clj
@@ -1,9 +1,9 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.8"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
+  :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
